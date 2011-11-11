8c818ef996778bc94fc1860b08a7e8da77692b1e
David Winslow <cdwinslow@gmail.com>
2011-11-10 20:22:57 -0500

    Update benchmark to be a bit more methodical

--- geocss/src/test/scala/Benchmark.scala
+++ geocss/src/test/scala/Benchmark.scala
@@ -1,35 +1,62 @@
 package org.geoscript.geocss
 
+import scala.collection.JavaConversions._
+import java.io.StringReader
+
 import org.geotools.{ styling => gt }
 import gt.Style
-import collection.JavaConversions._
 
 object Benchmark {
   val Translator = new Translator()
-  val template = """
-[%1$s < 10] {
-  stroke: black;
-  label: "%1$s - 1";
-}
-
-[%1$s > 20] {
-  stroke: red;
-  label: "%1$s - 2";
-}
-
-[%1$s < 20] {
-  stroke: blue;
-  label: "%1$s - 3";
-}
-
-[%1$s = 15] {
-  stroke: green;
-  label: "%1$s - 4";
-}
-"""
-
   val tx = new org.geotools.styling.SLDTransformer()
-  tx.setIndentation(4)
+
+  def main(args: Array[String]) {
+    println("Warming up...")
+    for (i <- 10 to 1 by -1) {
+      val hardCss = makeInconvenientCSS(2)
+      val worstCaseTime = time { 
+        val stylesheet = CssParser.parse(new StringReader(hardCss)).get
+        val style = Translator.css2sld(stylesheet)
+        encodeSLD(style)
+      }
+      print("%d..." format i)
+    }
+    println(" ok!")
+
+    // ok now test some stuff
+    for (i <- 1 to 4) {
+      val easyCss = makeConvenientCSS(i)
+      val hardCss = makeInconvenientCSS(i)
+      val bestCaseTime = { 
+        val parseTime = 0 // time { CssParser.parse(new StringReader(easyCss)).get }
+        val stylesheet = CssParser.parse(new StringReader(easyCss)).get
+        val rewriteTime = time { Translator.css2sld(stylesheet) }
+        val style = Translator.css2sld(stylesheet)
+        val encodeTime = 0 // time { encodeSLD(style) }
+        (parseTime, rewriteTime, encodeTime)
+      }
+      val worstCaseTime = { 
+        val parseTime = 0 // time { CssParser.parse(new StringReader(hardCss)).get }
+        val stylesheet = CssParser.parse(new StringReader(hardCss)).get
+        val rewriteTime = time { Translator.css2sld(stylesheet) }
+        val style = Translator.css2sld(stylesheet)
+        val encodeTime = 0 // time { encodeSLD(style) }
+        (parseTime, rewriteTime, encodeTime)
+      }
+
+      println("Easy: %s; Hard: %s" format (bestCaseTime, worstCaseTime))
+    }
+  }
+
+  def makeConvenientCSS(n: Int): String =
+    (for (i <- 1 to n)
+      yield "[PROP = %d] { fill: black; }" format i
+    ) mkString("\n")
+
+  def makeInconvenientCSS(n: Int): String =
+    (for (i <- 1 to n)
+      yield "[PROP_%d = 0] { fill: black; }" format i
+    ) mkString("\n")
 
   def encodeSLD(sld: gt.Style): String = {
     val out = new java.io.StringWriter()
@@ -37,49 +64,12 @@ object Benchmark {
     out.toString()
   }
 
-  def time[A](op: => A): (A, Long) = {
-    val startTime = System.currentTimeMillis()
-    (op, System.currentTimeMillis() - startTime)
-  }
-
-  def ruleCount(sld: Style): Int = {
-    sld.featureTypeStyles.foldLeft(0) { (i, fts) => i + fts.rules.length }
-  }
-
-  def main(args: Array[String]) {
-    for (end <- 'A' to 'B') {
-      // dry run; warm up the JIT statistics
-      encodeSLD(
-        Translator.css2sld(
-          CssParser.parse(
-            'A' to end map { template.format(_) } mkString
-          ).get
-        )
-      )
+  def time(op: => Unit): Double = {
+    val startTime = System.nanoTime()
+    var i = 1000
+    while ({i -= 1; i != 0}) {
+      op
     }
-
-    println("properties, parse_time, transform_time, rule_count, encode_time") 
-
-    for (end <- 'A' to 'D') {
-      val range = 1 + (end - 'A')
-      val css = 'A' to end map { template.format(_) } mkString
-      val (cssRules, parseTime) = time { CssParser.parse(css).get }
-      val (sldRules: Style, transformTime) = time { Translator.css2sld(cssRules) }
-      val (sld, encodeTime) = time { encodeSLD(sldRules) }
-      println(Seq(range, parseTime, transformTime, ruleCount(sldRules), encodeTime).mkString(", "))
-    }
-
-    println("values, parse_time, transform_time, rule_count, encode_time") 
-    for (range <- 4 to 16 by 4) {
-      val css = (1 to range) map {
-        """
-        [A=%1$s] { label: "%1$s"; }
-        """.format(_)
-      } mkString
-      val (cssRules, parseTime) = time { CssParser.parse(css).get }
-      val (sldRules, transformTime) = time { Translator.css2sld(cssRules) }
-      val (sld, encodeTime) = time { encodeSLD(sldRules) }
-      println(Seq(range, parseTime, transformTime, ruleCount(sldRules), encodeTime).mkString(", "))
-    }
+    (System.nanoTime() - startTime) / 1e9d
   }
 }
